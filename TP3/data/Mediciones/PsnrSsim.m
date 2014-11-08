920ad418922f616fe939b8687dd32dbde9ccac12
Agustina <agusaldasoro@gmail.com>
2014-11-08 13:12:38 -0300

    Informe++

--- TP3/data/Mediciones/PsnrSsim.m
+++ TP3/data/Mediciones/PsnrSsim.m
@@ -1,19 +1,33 @@
 img = imread('img12.bmp');
 imgVEC = imread('img12-vecino.bmp');
-psnrVEC = psnr(imgVEC, img);
-ssimVEC = ssim(imgVEC, img);
+        %psnrVEC = psnr(imgVEC, img);
+        %ssimVEC = ssim(imgVEC, img);
+A = img - imgVEC;
+        %A = imcomplement(A);
+imshow(A)
+        %imwrite(A, 'DIFFimg9VEC.jpeg')
+
 %% 
 
 img = imcrop(img, [2, 2, 765, 509]);
 imgBI = imread('img12-bilineal.bmp');
-psnrBI = psnr(imgBI, img);
-ssimBI = ssim(imgBI, img);
+        %psnrBI = psnr(imgBI, img);
+        %ssimBI = ssim(imgBI, img);
+B = img - imgBI;
+        %B = imcomplement(B);
+imshow(B)
+        %imwrite(B, 'DIFFimg1BI.jpeg')
+
 %% 
 
 img = imcrop(img, [2, 2, 763, 507]);
 imgMHC = imread('img12-MHC.bmp');
-psnrMHC = psnr(imgMHC, img);
-ssimMHC = ssim(imgMHC, img);
+        %psnrMHC = ps        nr(imgMHC, img);
+        %ssimMHC = ssim(imgMHC, img);
+C = img - imgMHC;
+        %C = imcomplement(C);
+imshow(C)
+        %imwrite(C, 'DIFFimg1MHC.jpeg')
 
 %% 
 psnrVEC
